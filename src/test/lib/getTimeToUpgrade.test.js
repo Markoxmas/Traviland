f1b47e6994c61b7ec7610c1915c3364605e2da8c
Marko Božič <marko.bozic@adacta-fintech.com>
2022-02-11 13:56:37 +0100

    Tests fix

--- src/test/lib/getTimeToUpgrade.test.js
+++ src/test/lib/getTimeToUpgrade.test.js
@@ -3,16 +3,23 @@ import getTimeToUpgrade from '../../lib/getTimeToUpgrade'
 describe('Tests for lib function getTimeToUpgrade', () => {
     it('should calculate time to upgrade correctly', () => {
         //Arrange
+        const now = new Date().getTime()
         const village = {
-            resources: {
-                clay: 600,
+            checkpoint: {
+                clay: 400,
                 wood: 300,
-                iron: 400,
+                iron: 500,
+                time: now,
             },
             production: {
                 clay: 100,
                 wood: 100,
                 iron: 100,
+            },
+            maxResources: {
+                clay: 5000,
+                wood: 5000,
+                iron: 5000,
             },
         }
         const cost = {
@@ -20,7 +27,7 @@ describe('Tests for lib function getTimeToUpgrade', () => {
             wood: 500,
             iron: 500,
         }
-        const expectedResult = 7200000 //ms
+        const expectedResult = 7200000 //ms - 2 hours
         //Act
         const result = getTimeToUpgrade(village, cost)
         //Assert
@@ -29,16 +36,23 @@ describe('Tests for lib function getTimeToUpgrade', () => {
 
     it('should return time in amount of zero when time is negative', () => {
         //Arrange
+        const now = new Date().getTime()
         const village = {
-            resources: {
+            checkpoint: {
                 clay: 1000,
                 iron: 1000,
                 wood: 1000,
+                time: now,
             },
             production: {
                 clay: 500,
                 wood: 400,
                 iron: 300,
+            },
+            maxResources: {
+                clay: 5000,
+                wood: 5000,
+                iron: 5000,
             },
         }
         const cost = {
