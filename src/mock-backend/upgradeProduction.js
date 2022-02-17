712ec3a8999a8f45602ee48a33e1b2694a59c931
Marko Božič <marko.bozic@adacta-fintech.com>
2022-02-17 08:59:21 +0100

    Changed production calculation

--- src/mock-backend/upgradeProduction.js
+++ src/mock-backend/upgradeProduction.js
@@ -1,30 +1,42 @@
 import getProductionInfo from '../lib/getProductionInfo'
 
 export default function upgradeProduction(village, serverConfig) {
+    const [brickyard, ironFoundry, sawmill] = [
+        village.fields.find((field) => field.type === 'brickyard'),
+        village.fields.find((field) => field.type === 'iron_foundry'),
+        village.fields.find((field) => field.type === 'sawmill'),
+    ]
+    const { BRICKYARD, IRON_FOUNDRY, SAWMILL } = serverConfig
     return {
-        clay: village.fields
-            .filter((field) => field.type === 'clay')
-            .reduce(
-                (production, field) =>
-                    production +
-                    getProductionInfo(serverConfig, field).currentLevel,
-                0
-            ),
-        wood: village.fields
-            .filter((field) => field.type === 'wood')
-            .reduce(
-                (production, field) =>
-                    production +
-                    getProductionInfo(serverConfig, field).currentLevel,
-                0
-            ),
-        iron: village.fields
-            .filter((field) => field.type === 'iron')
-            .reduce(
-                (production, field) =>
-                    production +
-                    getProductionInfo(serverConfig, field).currentLevel,
-                0
-            ),
+        clay: Math.floor(
+            village.fields
+                .filter((field) => field.type === 'clay')
+                .reduce(
+                    (production, field) =>
+                        production +
+                        getProductionInfo(serverConfig, field).currentLevel,
+                    0
+                ) * BRICKYARD.INCREASE[brickyard.level]
+        ),
+        wood: Math.floor(
+            village.fields
+                .filter((field) => field.type === 'wood')
+                .reduce(
+                    (production, field) =>
+                        production +
+                        getProductionInfo(serverConfig, field).currentLevel,
+                    0
+                ) * IRON_FOUNDRY.INCREASE[ironFoundry.level]
+        ),
+        iron: Math.floor(
+            village.fields
+                .filter((field) => field.type === 'iron')
+                .reduce(
+                    (production, field) =>
+                        production +
+                        getProductionInfo(serverConfig, field).currentLevel,
+                    0
+                ) * SAWMILL.INCREASE[sawmill.level]
+        ),
     }
 }
